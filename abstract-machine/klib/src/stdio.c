7ff727a8a213307e959b0319dbe4f2b15e21ecd2
thanatos <2097712819@qq.com>
2025-09-11 16:29:46 +0800

    添加了vsprintf和printf的实现，修改了rem求余操作

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -7,15 +7,85 @@
 
 int printf(const char *fmt, ...)
 {
-  panic("Not implemented");
+#define PRINT_MAX_SIZE 1024
+  char out[PRINT_MAX_SIZE];
+  va_list args;
+  va_start(args, fmt);
+  int ret = vsprintf(out, fmt, args);
+  for (int i = 0; (i < ret) ; i++)
+  {
+    putch(out[i]);
+  }
+  va_end(args);
+  return strlen(out);
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap)
 {
+  char *s;
+  int d;
+  int count = 0;
+  while (*fmt)
+  {
+    char c = *fmt++;
+    if (c == '%')
+    {
+      c = *fmt++;
+      switch (c)
+      {
+      case 's':
+        s = va_arg(ap, char *);
+        memmove(out + count, s, strlen(s));
+        count += strlen(s);
+        break;
+      case 'd':
+      {
+        d = va_arg(ap, int);
+        char str[256];
+        itoa(d, str);
+        memmove(out + count, str, strlen(str));
+        count += strlen(str);
+        free(str);
+        break;
+      }
+      case 'c':
+        char x = (char)va_arg(ap,int);
+        out[count++] = x;
+        break;
+      default:
+        out[count++] = c;
+        break;
+      }
+    }
+    else
+    {
+      out[count++] = c;
+    }
+  }
+  out[count] = '\0';
+  return count;
+}
+
+int sprintf(char *out, const char *fmt, ...)
+{
+  va_list args;
+  va_start(args, fmt);
+  int count = vsprintf(out, fmt, args);
+  va_end(args);
+  return count;
+}
+
+int snprintf(char *out, size_t n, const char *fmt, ...)
+{
   panic("Not implemented");
 }
 
-char *itoa(int s, char *str)
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap)
+{
+  panic("Not implemented");
+}
+
+void itoa(int s, char *str)
 {
   size_t i = 0;
   if (s < 0)
@@ -36,61 +106,5 @@ char *itoa(int s, char *str)
     str[i++] = a[count--] + '0';
   }
   str[i] = '\0';
-  return str;
 }
-
-int sprintf(char *out, const char *fmt, ...)
-{
-  va_list args;
-  va_start(args, fmt);
-  char *s;
-  int d;
-  int count = 0;
-  while (*fmt)
-  {
-    char c = *fmt++;
-    if (c == '%')
-    {
-      c = *fmt++;
-      switch (c)
-      {
-      case 's':
-        s = va_arg(args, char *);
-        memmove(out + count, s, strlen(s));
-        count += strlen(s);
-        break;
-      case 'd':
-      {
-        d = va_arg(args, int);
-        char str[256]; 
-        itoa(d, str);
-        memmove(out + count, str, strlen(str));
-        count += strlen(str);
-        free(str);
-        break;
-      }
-      default:
-        out[count++] = c;
-        break;
-      }
-    }
-    else
-    {
-      out[count++] = c;
-    }
-  }
-  out[count] = '\0';
-  return count;
-}
-
-int snprintf(char *out, size_t n, const char *fmt, ...)
-{
-  panic("Not implemented");
-}
-
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap)
-{
-  panic("Not implemented");
-}
-
 #endif
